56420f2d95070ca03314c664372be71c768028b8
Emiliano Martinez <emi.martinez9696@gmail.com>
2023-03-14 12:29:45 -0300

    Debugging directory name in uploadImage.js

--- util/uploadImage.js
+++ util/uploadImage.js
@@ -2,6 +2,8 @@ async function uploadImage(img){
     const dir = __dirname.slice(0,__dirname.length-5)
     let uploadPath = `${dir}\\images\\${img.name}`
 
+    console.log(dir)
+
     try {
         await img.mv(uploadPath)
     } catch (error) {
