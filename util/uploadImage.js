18aa80c6c037b81035fef4875add7ca72880db10
Emiliano Martinez <emi.martinez9696@gmail.com>
2023-03-14 12:48:17 -0300

    Full directory name in console uploadImage.js

--- util/uploadImage.js
+++ util/uploadImage.js
@@ -2,7 +2,7 @@ async function uploadImage(img){
     const dir = __dirname.slice(0,__dirname.length-5)
     let uploadPath = `${dir}\\images\\${img.name}`
 
-    console.log(dir)
+    console.log(_dirname)
 
     try {
         await img.mv(uploadPath)
