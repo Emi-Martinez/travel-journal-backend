0cdf6b154fa2313ddb941c153b9ade3bfa0c8ff0
Emiliano Martinez <emi.martinez9696@gmail.com>
2023-03-13 21:49:36 -0300

    Ssl added to Connection.js

--- db/Connection.js
+++ db/Connection.js
@@ -5,7 +5,10 @@ const connection = mysql.createConnection(
         host: process.env.DB_HOST || 'localhost',
         user: process.env.DB_USER || 'root',
         database: process.env.DB_NAME || 'travel_journal',
-        password: process.env.DB_PASSWORD || ''
+        password: process.env.DB_PASSWORD || '',
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 )
 
--- models/Location.js
+++ models/Location.js
@@ -1,4 +1,4 @@
-const con = require('../db/Conection')
+const con = require('../db/Connection')
 
 class Location {
     constructor(loc) {
