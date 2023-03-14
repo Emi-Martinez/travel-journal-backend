f08e30dfa26d7677c7355c8529da9bffb83fc928
Emiliano Martinez <emi.martinez9696@gmail.com>
2023-03-14 15:41:58 -0300

    Removing console.log for directory name

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const express = require('express')
 const {route} = require('./routes/locations')
 const fileUpload = require('express-fileupload')
 const PORT = process.env.PORT || 3001
-const dir = __dirname.slice(0, __dirname.length-5)
-let uploadPath = `${dir}/images/nombreImage`
-
-console.log(dir.slice(0,dir.length-5))
-console.log(uploadPath)
-// console.log(__filename)
-
 
 const app = express()
 
