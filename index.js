fbdb98e226d59a8f6ad36fb63d6aa0abd1557177
Emiliano Martinez <emi.martinez9696@gmail.com>
2023-03-17 18:58:18 -0300

    dotenv dependency added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('dotenv').config()
 const cors = require('cors')
 const {route} = require('./routes/locations')
 const fileUpload = require('express-fileupload')
